9b140acbfe8e099c3893af763227116e06039ae6
carlosrod061 <carlosrod061@gmail.com>
2021-12-08 13:46:20 -0600

    Acceso a Ubicación
    Funcionalidad de ubicación con trazado de rutas en el apartado de contactos.

--- assets/js/12ModalContacto2.js
+++ assets/js/12ModalContacto2.js
@@ -1,122 +1,157 @@
 $(document).ready(function () {
+    if (navigator.geolocation) { //check if geolocation is available
+        var latitude;
+        var longitude;
+        navigator.geolocation.getCurrentPosition(function (position) {
 
-    function modalContraseniasNoCoinciden(){
-        $(".modal-header").css("background-color", "#34b7a7");
-        $(".modal-header").css("color", "white");
-        $(".modal-title").text("Cuidado"); 
-        $(".label1").text("Las constraseñas no coinciden"); 
-        $(".label2").text("Intentalo de nuevo");           
-        $("#modalContacto12").modal("show"); 
-        $('#btnAceptarModal').click(function () {
-            $('#modalContacto12').modal('hide'); 
-          });
+            latitude = position.coords.latitude;
+            longitude = position.coords.longitude;
+            var map = L.map('map').setView([latitude, longitude], 100);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+
+            L.marker([25.677621592958456, -100.29497681603686]).addTo(map)
+            .bindPopup('Active Office')
+            .openPopup();
+
+            L.marker([latitude, longitude]).addTo(map)
+                .bindPopup('Tu localización')
+                .openPopup();
+
+           
+
+                L.Routing.control({
+                    waypoints: [
+                      L.latLng(latitude, longitude),
+                      L.latLng(25.677621592958456, -100.29497681603686)
+                    ]
+                  }).addTo(map);
+
+        });
     }
-    function modalResgistrado(){
+
+    
+
+    function modalContraseniasNoCoinciden() {
         $(".modal-header").css("background-color", "#34b7a7");
         $(".modal-header").css("color", "white");
-        $(".modal-title").text("Excelente"); 
+        $(".modal-title").text("Cuidado");
+        $(".label1").text("Las constraseñas no coinciden");
+        $(".label2").text("Intentalo de nuevo");
+        $("#modalContacto12").modal("show");
+        $('#btnAceptarModal').click(function () {
+            $('#modalContacto12').modal('hide');
+        });
+    }
+    function modalResgistrado() {
+        $(".modal-header").css("background-color", "#34b7a7");
+        $(".modal-header").css("color", "white");
+        $(".modal-title").text("Excelente");
         $(".label1").text("El usuario se ha registrado con éxito");
-        $(".label2").text(""); 
-        $("#modalContacto12").modal("show"); 
+        $(".label2").text("");
+        $("#modalContacto12").modal("show");
         $('#btnAceptarModal').click(function () {
-            $('#modalContacto12').modal('hide'); 
-            window.location.href ='5_cuenta.php';
+            $('#modalContacto12').modal('hide');
+            window.location.href = '5_cuenta.php';
             //location.reload(); 
-          });
+        });
     }
 
-    function modalNoResgistrado(){
+    function modalNoResgistrado() {
         $(".modal-header").css("background-color", "#34b7a7");
         $(".modal-header").css("color", "white");
-        $(".modal-title").text("El usuario ya está registrado"); 
-        $(".label1").text("El correo que deseas ingresar ya ha sido registrado antes, intenta con otro."); 
-        $(".label2").text(""); 
-        $("#modalContacto12").modal("show"); 
+        $(".modal-title").text("El usuario ya está registrado");
+        $(".label1").text("El correo que deseas ingresar ya ha sido registrado antes, intenta con otro.");
+        $(".label2").text("");
+        $("#modalContacto12").modal("show");
         $('#btnAceptarModal').click(function () {
-            $('#modalContacto12').modal('hide'); 
-          });
+            $('#modalContacto12').modal('hide');
+        });
     }
 
-    function modalFormatoCorreoIncorrecto(email){
+    function modalFormatoCorreoIncorrecto(email) {
         $(".modal-header").css("background-color", "#34b7a7");
         $(".modal-header").css("color", "white");
-        $(".modal-title").text("Cuidado"); 
-        $(".label1").text("El formato del correo "+email+" es erróneo."); 
-        $(".label2").text("Ingresa una válido");           
-        $("#modalContacto12").modal("show"); 
+        $(".modal-title").text("Cuidado");
+        $(".label1").text("El formato del correo " + email + " es erróneo.");
+        $(".label2").text("Ingresa una válido");
+        $("#modalContacto12").modal("show");
         $('#btnAceptarModal').click(function () {
-            $('#modalContacto12').modal('hide'); 
-          });
+            $('#modalContacto12').modal('hide');
+        });
     }
 
-    function modalFormatoNombreIncorrecto(name){
+    function modalFormatoNombreIncorrecto(name) {
         $(".modal-header").css("background-color", "#34b7a7");
         $(".modal-header").css("color", "white");
-        $(".modal-title").text("Cuidado"); 
-        $(".label1").text("El nombre "+name+" es muy corto."); 
-        $(".label2").text("El nombre debe tener mínimo 4 letras, intentalo de nuevo");           
-        $("#modalContacto12").modal("show"); 
+        $(".modal-title").text("Cuidado");
+        $(".label1").text("El nombre " + name + " es muy corto.");
+        $(".label2").text("El nombre debe tener mínimo 4 letras, intentalo de nuevo");
+        $("#modalContacto12").modal("show");
         $('#btnAceptarModal').click(function () {
-            $('#modalContacto12').modal('hide'); 
-          });
+            $('#modalContacto12').modal('hide');
+        });
     }
 
-    function modalFormatoTelefonoIncorrecto(tel){
+    function modalFormatoTelefonoIncorrecto(tel) {
         $(".modal-header").css("background-color", "#34b7a7");
         $(".modal-header").css("color", "white");
-        $(".modal-title").text("Cuidado"); 
-        $(".label1").text("El teléfono "+tel+" es muy corto."); 
-        $(".label2").text("El teléfono debe tener mínimo 10 dígitos, intentalo de nuevo");           
-        $("#modalContacto12").modal("show"); 
+        $(".modal-title").text("Cuidado");
+        $(".label1").text("El teléfono " + tel + " es muy corto.");
+        $(".label2").text("El teléfono debe tener mínimo 10 dígitos, intentalo de nuevo");
+        $("#modalContacto12").modal("show");
         $('#btnAceptarModal').click(function () {
-            $('#modalContacto12').modal('hide'); 
-          });
+            $('#modalContacto12').modal('hide');
+        });
     }
 
-    function modalFormatoContrasIncorrecto(){
+    function modalFormatoContrasIncorrecto() {
         $(".modal-header").css("background-color", "#34b7a7");
         $(".modal-header").css("color", "white");
-        $(".modal-title").text("Cuidado"); 
-        $(".label1").text("La contraseña es muy corta."); 
-        $(".label2").text("La contraseña debe tener mínimo 8 caracteres, intentalo de nuevo");           
-        $("#modalContacto12").modal("show"); 
+        $(".modal-title").text("Cuidado");
+        $(".label1").text("La contraseña es muy corta.");
+        $(".label2").text("La contraseña debe tener mínimo 8 caracteres, intentalo de nuevo");
+        $("#modalContacto12").modal("show");
         $('#btnAceptarModal').click(function () {
-            $('#modalContacto12').modal('hide'); 
-          });
+            $('#modalContacto12').modal('hide');
+        });
     }
 
-    function modalFormatoUltimoCampoIncorrecto(){
+    function modalFormatoUltimoCampoIncorrecto() {
         $(".modal-header").css("background-color", "#34b7a7");
         $(".modal-header").css("color", "white");
-        $(".modal-title").text("Cuidado"); 
-        $(".label1").text("El texto del último campo es muy corto."); 
-        $(".label2").text("El campo debe tener mínimo 7 caracteres, intentalo de nuevo");           
-        $("#modalContacto12").modal("show"); 
+        $(".modal-title").text("Cuidado");
+        $(".label1").text("El texto del último campo es muy corto.");
+        $(".label2").text("El campo debe tener mínimo 7 caracteres, intentalo de nuevo");
+        $("#modalContacto12").modal("show");
         $('#btnAceptarModal').click(function () {
-            $('#modalContacto12').modal('hide'); 
-          });
+            $('#modalContacto12').modal('hide');
+        });
     }
 
-    function modalCampoMensajeObligatorio(campo){
+    function modalCampoMensajeObligatorio(campo) {
         $(".modal-header").css("background-color", "#34b7a7");
         $(".modal-header").css("color", "white");
-        $(".modal-title").text("Cuidado"); 
-        $(".label1").text("Obligatorio"); 
-        $(".label2").text("El "+campo+" es obligatorio");           
-        $("#modalContacto12").modal("show"); 
+        $(".modal-title").text("Cuidado");
+        $(".label1").text("Obligatorio");
+        $(".label2").text("El " + campo + " es obligatorio");
+        $("#modalContacto12").modal("show");
         $('#btnAceptarModal').click(function () {
-            $('#modalContacto12').modal('hide'); 
-          });
+            $('#modalContacto12').modal('hide');
+        });
     }
 
-    
-    
+
+
     $('#close').click(function () {
-        $('#modalContacto12').modal('hide'); 
-      });
-    
+        $('#modalContacto12').modal('hide');
+    });
+
     $('#btnInscripcion').click(function () {
-            
+
         var name = document.getElementById('name').value;
         var email = document.getElementById('email').value;
         var tel = document.getElementById('tel').value;
@@ -125,18 +160,18 @@ $(document).ready(function () {
 
         var question = document.getElementById('question').value;
         var answer = document.getElementById('qu').value;
-        
+
         var message = document.getElementById('message').value;
         var expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-        var camp ="";
+        var camp = "";
         //console.log(name.length);
         //nameContado = name.length;
-        if(name.length>=4){
-            if(expr.test(email)){
-                if(tel.length>=10){
-                    if(pass1.length>=8 && pass2.length>=8 && pass1 == pass2){
-                        if(message != "" && message.length>=7){
-                            if(answer != "" && answer.length>=3){
+        if (name.length >= 4) {
+            if (expr.test(email)) {
+                if (tel.length >= 10) {
+                    if (pass1.length >= 8 && pass2.length >= 8 && pass1 == pass2) {
+                        if (message != "" && message.length >= 7) {
+                            if (answer != "" && answer.length >= 3) {
                                 //alert("Todos los campos se validaron bien");
                                 $.post('assets/js/registro.php', {
                                     "name": name,
@@ -146,91 +181,91 @@ $(document).ready(function () {
                                     "message": message,
                                     "question": question,
                                     "answer": answer
-                                  },function(data) {
+                                }, function (data) {
                                     console.log('procesamiento finalizado', data);
-                                    if(data ==1){
+                                    if (data == 1) {
                                         modalNoResgistrado();
                                         document.getElementById("email").value = "";
                                     }
-                                    else if(data ==2){
-                                        
+                                    else if (data == 2) {
+
                                         modalResgistrado();
-                                        document.getElementById('name').value="";
-                                        document.getElementById('email').value="";
-                                        document.getElementById('tel').value="";
-                                        document.getElementById('pass1').value="";
-                                        document.getElementById('pass2').value="";
-                                        document.getElementById('message').value="";
+                                        document.getElementById('name').value = "";
+                                        document.getElementById('email').value = "";
+                                        document.getElementById('tel').value = "";
+                                        document.getElementById('pass1').value = "";
+                                        document.getElementById('pass2').value = "";
+                                        document.getElementById('message').value = "";
                                     }
                                 });
                             }
-                            else{
+                            else {
                                 modalFormatoUltimoCampoIncorrecto();
                             }
                         }
-                        else{
-                            camp="último"
+                        else {
+                            camp = "último"
                             modalCampoMensajeObligatorio(camp);
                         }
                     }
-                    else{
-                        if(pass1 == "" || pass2 == ""){
-                            camp="campo de las contraseñas"
+                    else {
+                        if (pass1 == "" || pass2 == "") {
+                            camp = "campo de las contraseñas"
                             modalCampoMensajeObligatorio(camp);
 
                         }
-                        else{
-                            if(pass1.length>=8 && pass2.length>=8 && pass1 != pass2){
+                        else {
+                            if (pass1.length >= 8 && pass2.length >= 8 && pass1 != pass2) {
                                 modalContraseniasNoCoinciden();
                                 document.getElementById("pass1").value = "";
                                 document.getElementById("pass2").value = "";
                             }
-                            else if(pass1.length<8 || pass2.length<8){
+                            else if (pass1.length < 8 || pass2.length < 8) {
                                 modalFormatoContrasIncorrecto();
                             }
 
                         }
-                        
+
                     }
 
                 }
-                else{
-                    if(tel==""){
-                        camp="teléfono"
+                else {
+                    if (tel == "") {
+                        camp = "teléfono"
                         modalCampoMensajeObligatorio(camp);
                     }
-                    else{
+                    else {
                         modalFormatoTelefonoIncorrecto(tel);
                     }
                 }
 
             }
-            else{
-                if(email==""){
-                    camp="correo electrónico"
+            else {
+                if (email == "") {
+                    camp = "correo electrónico"
                     modalCampoMensajeObligatorio(camp);
                 }
-                else{
+                else {
                     modalFormatoCorreoIncorrecto(email);
                 }
             }
-            
+
         }
-        else{
-            if(name == ""){
-                camp="nombre"
+        else {
+            if (name == "") {
+                camp = "nombre"
                 modalCampoMensajeObligatorio(camp);
             }
-            else{
+            else {
                 modalFormatoNombreIncorrecto(name);
             }
         }
 
         //if ( !expr.test(email) ){//COMPRUEBA MAIL
         //    modalFormatoCorreoIncorrecto(email)
-            //alert("Error: La dirección de correo " + email + " es incorrecta.");
+        //alert("Error: La dirección de correo " + email + " es incorrecta.");
         //}
-        
+
         //console.log(name.length);
 
         //if(name.lenght>=4 && email.lenght){
@@ -238,42 +273,41 @@ $(document).ready(function () {
         //}
 
         //if(pass1 != pass2){
-            //alert('Las constrasenas no coinciden');
-         //   modalContraseniasNoCoinciden()
-         //   document.getElementById("pass1").value = "";
-         //   document.getElementById("pass2").value = "";
-    
-       // }
+        //alert('Las constrasenas no coinciden');
+        //   modalContraseniasNoCoinciden()
+        //   document.getElementById("pass1").value = "";
+        //   document.getElementById("pass2").value = "";
+
+        // }
 
         //if(pass1 == pass2){
-            /** 
-            $.post('assets/js/registro.php', {
-                "name": name,
-                "tel": tel,
-                "email": email,
-                "pass1": pass1,
-                "message": message
-              },function(data) {
-                console.log('procesamiento finalizado', data);
-                if(data ==1){
-                    modalNoResgistrado();
-                    document.getElementById("email").value = "";
-                }
-                else if(data ==2){
-                    
-                    modalResgistrado();
-                    
-                }
-            });
-            */
+        /** 
+        $.post('assets/js/registro.php', {
+            "name": name,
+            "tel": tel,
+            "email": email,
+            "pass1": pass1,
+            "message": message
+          },function(data) {
+            console.log('procesamiento finalizado', data);
+            if(data ==1){
+                modalNoResgistrado();
+                document.getElementById("email").value = "";
+            }
+            else if(data ==2){
+                
+                modalResgistrado();
+                
+            }
+        });
+        */
         //}
 
 
-          
+
     });
-    
+
 
 });
 
 
- 
